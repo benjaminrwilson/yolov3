8b359b10532e4d796a690ce553780e8111500d70
Benjamin R. Wilson <benjaminrwilson@icloud.com>
2018-12-21 12:16:11 -0500

    updated requirements and setup

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='yolov3',
       version='0.1',
       description='YOLOv3 implementation in PyTorch',
       url='https://github.com/benjaminrwilson/yolov3',
       author='Benjamin Wilson',
       license='MIT',
+      install_requires=requirements,
       packages=find_packages(),
       zip_safe=False)
