8aa53f523762418d54afe54f9ff85d9d2d0f18d5
Benjamin R. Wilson <benjaminrwilson@icloud.com>
2018-12-22 00:54:09 -0500

    bug fix

--- yolov3/tools/get_models.sh
+++ yolov3/tools/get_models.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if ! test -d ~/.torch/yolov3/; then mkdir ~/.torch/yolov3/; fi
+if ! test -d ~/.torch/yolov3/; then mkdir -p ~/.torch/yolov3/; fi
 
 wget https://pjreddie.com/media/files/yolov3.weights -O ~/.torch/yolov3/yolov3.weights
