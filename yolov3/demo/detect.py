c0e1049e3663da9c57f2679c3ef743a0b344e5ea
Benjamin R. Wilson <benjaminrwilson@icloud.com>
2018-12-30 10:16:31 -0500

    initial refactor

--- yolov3/demo/detect.py
+++ yolov3/demo/detect.py
@@ -9,13 +9,12 @@ from torch import optim
 from tqdm import tqdm
 
 from yolov3.src import models, utils
-from yolov3.src.data import YoloDataset
+from yolov3.src.datasets import YoloDataset
 from yolov3.src.get_image_size import get_image_size
 from yolov3.src.losses import YoloLoss
 
 
 def test(opts):
-    # Create model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     dataset = YoloDataset(opts.src, opts.size)
@@ -24,116 +23,40 @@ def test(opts):
                                              shuffle=True,
                                              num_workers=4)
 
-    training = False
-    if opts.mode == "training":
-        training = True
-
     model = models.Darknet(opts.cfg, opts.weights,
                            opts.nms, opts.obj,
-                           opts.size, device,
-                           training).to(device)
+                           opts.size, device, False).to(device).eval()
 
-    if not training:
-        model.eval()
+    run_detect(model, dataloader, opts, device)
 
+
+def run_detect(model, dataloader, opts, device):
+    results = {}
     class_colors = utils.generate_class_colors(model.num_classes)
     class_to_names = utils.get_class_names(opts.names_path)
     with torch.no_grad():
-        if opts.mode == "images":
-            run_detect(model, dataloader, opts,
-                       class_colors, class_to_names, device)
-        elif opts.mode == "cam":
-            run_cam_detect(model,
-                           opts,
-                           class_colors,
-                           class_to_names,
-                           device)
-        elif opts.mode == "training":
-            run_train(model, dataloader, opts, device)
-        elif opts.mode == "map":
-            run_detect(model,
-                       dataloader,
-                       opts,
-                       class_colors,
-                       class_to_names,
-                       device,
-                       True)
-
-
-def run_detect(model, dataloader, opts, class_colors,
-               class_to_names,
-               device,
-               use_map=False):
-    results = {}
-    for i, (img, img_name) in enumerate(tqdm(dataloader)):
-        start_time = time.time()
-        img = img.to(device)
-        detections = model(img)
-        if len(detections) > 0:
-            abs_img_path = os.path.join(opts.src, img_name[0])
-            width, height = get_image_size(abs_img_path)
-            detections = utils.transform_detections(
-                detections, width, height, opts.size)
-            detections = detections.view(-1, 7)
-            confidences = detections[..., -2] * detections[..., -3]
-            detections = torch.cat((detections[..., 6].unsqueeze(1),
+        for i, (img, img_name) in enumerate(tqdm(dataloader)):
+            start_time = time.time()
+            img = img.to(device)
+            detections = model(img)
+            if detections.shape[0] > 0:
+                width, height = get_image_size(img_name[0])
+                detections = utils.transform_detections(
+                    detections, width, height, opts.size)
+                detections = detections.view(-1, 7)
+                confidences = detections[..., -2] * detections[..., -3]
+                detections = torch.cat((detections[..., 6].unsqueeze(1),
                                         confidences.unsqueeze(1),
                                         detections[..., :4]), 1)
-        if use_map:
-            results[img_name[0]] = detections
-        else:
-            utils.write_detections(
-                detections,
-                img_name[0],
-                opts.size,
-                opts.dst,
-                class_colors,
-                class_to_names)
+        utils.write_detections(
+            detections,
+            img_name[0],
+            opts.size,
+            opts.dst,
+            class_colors,
+            class_to_names)
         elapsed = round(time.time() - start_time, 2)
         info = "Processed image {} in {} seconds".format(i, elapsed)
-
-    if use_map:
-        print("Not Implemented")
-        return
-        # utils.calculate_map(opts.ann_path, opts.src, results, device,
-        #                     class_colors, class_to_names, opts.size, opts.dst)
-
-
-def run_cam_detect(model, opts, class_colors, class_to_names, device):
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        if ret:
-            start_time = time.time()
-            detections = model.detect(frame)
-            utils.write_detections_cam(
-                detections, frame, opts.size, class_colors, class_to_names)
-            utils.write_fps(frame, start_time)
-
-            cv2.imshow('img', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-
-def run_map(model, dataloader, opts):
-    pass
-
-
-def run_train(model, dataloader, opts, device):
-    criterion = YoloLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.05)
-    for i, (img, img_name) in enumerate(dataloader):
-        start_time = time.time()
-        img = img.to(device)
-        ann_name = img_name[0].split("/")[-1].split(".")[0] + ".txt"
-        abs_img_path = os.path.join(opts.src, img_name[0])
-
-        frame = cv2.imread(abs_img_path)
-        targets = utils.get_targets(opts, ann_name, img_name[0])
-        print(targets.shape)
-
-        loss = model(img, targets, frame)
-        return
 
 
 def get_model(opts):
@@ -173,10 +96,8 @@ def get_args():
     opts.add_argument(
         '-d', '--dst', help='Destination directory', default="../results")
     opts.add_argument(
-        '-m', '--mode', help='Use video camera for demo', default="cam")
-    opts.add_argument(
         '-np', '--names_path', help='Path to names of classes',
-        default="../config/coco.names")
+        default="../data/coco.names")
     opts.add_argument(
         '-a', '--ann_path', help='Path to annotations of the images',
         default="../annotations/")
