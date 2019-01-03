25a96f0b2edae603bf945ebc1c53ee2467635fb6
Benjamin R. Wilson <benjaminrwilson@icloud.com>
2019-01-02 22:14:05 -0500

    small path update

--- yolov3/tools/get_coco.sh
+++ yolov3/tools/get_coco.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if ! [ test -d ~/datasets ]; then
-    mkdir -p ~/datasets/coco
+if ! [ test -d ~/coco ]; then
+    mkdir -p ~/coco
 else
-    echo "'datasets' directory exists, please remove or edit the bash script!"
+    echo "'coco' directory exists, please remove or edit the bash script!"
     exit
 fi
 
-wget -c https://pjreddie.com/media/files/val2014.zip -P ~/datasets/coco
-wget -c https://pjreddie.com/media/files/instances_train-val2014.zip -P ~/datasets/coco
+wget -c https://pjreddie.com/media/files/val2014.zip -P ~/coco
+wget -c https://pjreddie.com/media/files/instances_train-val2014.zip -P ~/coco
